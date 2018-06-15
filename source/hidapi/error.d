c96f5068b08d79d25fed9fe4a1aeed42eead56b6
FFY00 <filipe.lains@gmail.com>
2018-06-15 16:47:56 +0100

    hidapid: major update v1.0.1
    No more ugly callbacks.

--- source/hidapi/error.d
+++ source/hidapi/error.d
@@ -1,7 +1,13 @@
 module hidapi.error;
 
-class HidError : Exception {
+class HidError : Exception
+{
+
+    /***********************************
+     * Default constructor
+     */
     this(string msg, string file = __FILE__, size_t line = __LINE__) {
         super(msg, file, line);
     }
+
 }
---
+++ source/hidapi/hid.d
@@ -0,0 +1,32 @@
+module hidapi.hid;
+
+import hidapi.bindings;
+import hidapi.error;
+import std.exception : enforce;
+
+class Hid
+{
+
+    /***********************************
+     * Inits the HIDAPI 
+     */
+    shared static this()
+    {
+        int ret = hid_init();
+        enforce!HidError(ret != -1, "Failed to init HIDAPI");
+    }
+
+    /***********************************
+     * Exits the HIDAPI
+     *
+     * This is generating an error so
+     * it's disabled at the moment.
+     */
+    shared static ~this()
+    {
+        //int ret = hid_exit();
+        int ret = 0;
+        enforce!HidError(ret != -1, "Failed to exit HIDAPI");
+    }
+
+}
